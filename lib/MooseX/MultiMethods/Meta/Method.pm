a387b05b53a8df41c5e75ae066d183f7b4d8e036
Florian Ragwitz <rafl@debian.org>
2009-04-13 04:14:13 +0000

    Import of FLORA/MooseX-MultiMethods-0.01 from CPAN.
    gitpan-cpan-distribution: MooseX-MultiMethods
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         FLORA/MooseX-MultiMethods-0.01.tar.gz
    gitpan-cpan-author:       FLORA
    gitpan-cpan-maturity:     released

---
+++ lib/MooseX/MultiMethods/Meta/Method.pm
@@ -0,0 +1,66 @@
+package MooseX::MultiMethods::Meta::Method;
+our $VERSION = '0.01';
+
+
+use Moose;
+use MooseX::Method::Signatures;
+use MooseX::Types::Moose qw/CodeRef/;
+use Devel::PartialDump qw/dump/;
+use aliased 'MooseX::Types::VariantTable';
+
+use namespace::autoclean;
+
+extends 'Moose::Object', 'Moose::Meta::Method';
+
+has _variant_table => (
+    is      => 'ro',
+    isa     => VariantTable,
+    default => sub { VariantTable->new },
+    handles => [qw/add_variant/],
+);
+
+has body => (
+    is      => 'ro',
+    isa     => CodeRef,
+    lazy    => 1,
+    builder => 'initialize_body',
+);
+
+method initialize_body {
+    my $name          = $self->name;
+    my $variant_table = $self->_variant_table;
+
+    return sub {
+        my ($args) = \@_;
+
+        if (my ($result, $type) = $variant_table->find_variant($args)) {
+            my $method = $result->body;
+            goto $method;
+        }
+
+        confess "no variant of method '${name}' found for ", dump($args);
+    };
+}
+
+1;
+
+__END__
+=head1 NAME
+
+MooseX::MultiMethods::Meta::Method
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+  Florian Ragwitz <rafl@debian.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Florian Ragwitz.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
